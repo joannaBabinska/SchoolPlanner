72e094e64526cd27c8cf2214d88e9a5b8c36700c
Joanna Babinska <babinskaasia52@gmail.com>
2023-04-13 07:05:31 +0200

    first commit

---
+++ src/main/java/com/example/schoolplaner/SchoolPlanerApplication.java
@@ -0,0 +1,13 @@
+package com.example.schoolplaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolPlanerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolPlanerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/schoolplaner/SchoolPlanerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schoolplaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolPlanerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
