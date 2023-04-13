6ff056a407e5efa005eb28844640d7461823cefa
Joanna Babinska <babinskaasia52@gmail.com>
2023-04-13 07:27:19 +0200

    add testdata.sql

---
+++ src/main/resources/db/testdata/testdata.sql
@@ -0,0 +1,5 @@
+INSERT INTO
+    teacher(id,first_name, last_name, date_of_birth, hourly_wage, email, phone_number)
+VALUES
+    (1,'Jan', 'Kowalski', '1999-10-12', 100, 'joanna@gmail.com', '889655425'),
+    (2,'Joanna', 'Nowak', '1999-06-21', 95, 'kacper@gamil.com', '962158745');
