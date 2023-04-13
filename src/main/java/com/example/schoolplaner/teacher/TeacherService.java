f680ea29514ce9d97fb6aa1d515a47b547f8fe7d
Joanna Babinska <babinskaasia52@gmail.com>
2023-04-13 07:17:17 +0200

    add getTeacherById method in TeacherService.java

--- src/main/java/com/example/schoolplaner/teacher/TeacherService.java
+++ src/main/java/com/example/schoolplaner/teacher/TeacherService.java
@@ -3,9 +3,15 @@ package com.example.schoolplaner.teacher;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor
 public class TeacherService {
 
-    private final  TeacherRepository teacherRepository;
+    private final TeacherRepository teacherRepository;
+
+    Optional<Teacher> getTeacherById(Long id) {
+        return teacherRepository.findById(id);
+    }
 }
