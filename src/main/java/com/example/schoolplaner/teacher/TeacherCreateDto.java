021317fb5bdd32894fcbe0b43a773c2a4b3fc575
Joanna Babinska <babinskaasia52@gmail.com>
2023-04-13 07:45:48 +0200

    add TeacherCreateDto.java

---
+++ src/main/java/com/example/schoolplaner/teacher/TeacherCreateDto.java
@@ -0,0 +1,25 @@
+package com.example.schoolplaner.teacher;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+@Data
+public class TeacherCreateDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private BigDecimal hourlyWage;
+    private String email;
+    private String phoneNumber;
+
+    public TeacherCreateDto(String firstName, String lastName, LocalDate dateOfBirth, BigDecimal hourlyWage, String email, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.hourlyWage = hourlyWage;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+}
