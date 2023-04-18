3cc1e7730792c3ab907af2c69ed2a5b48e0435e9
Joanna Babinska <babinskaasia52@gmail.com>
2023-04-18 09:37:42 +0200

    modify EmailExistException.java and TeacherNotFoundException.java

--- src/main/java/com/example/schoolplanner/teacher/exception/EmailExistException.java
+++ src/main/java/com/example/schoolplanner/teacher/exception/EmailExistException.java
@@ -1,14 +1,9 @@
 package com.example.schoolplanner.teacher.exception;
 
 public class EmailExistException extends RuntimeException {
-    private final String message;
-    public EmailExistException(String email) {
-        message = "Email %s already exist".formatted(email);
-    }
 
-    @Override
-    public String getMessage() {
-        return message;
+    public EmailExistException(String email) {
+        String message = "Email %s already exist".formatted(email);
     }
-    //todo
 }
+
