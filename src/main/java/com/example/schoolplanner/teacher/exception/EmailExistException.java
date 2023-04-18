e765ffdcc2e1f5d2db5b17d902fe6d49bc37c650
Joanna Babinska <babinskaasia52@gmail.com>
2023-04-18 11:38:33 +0200

    modify EmailExistException.java

--- src/main/java/com/example/schoolplanner/teacher/exception/EmailExistException.java
+++ src/main/java/com/example/schoolplanner/teacher/exception/EmailExistException.java
@@ -1,9 +1,9 @@
 package com.example.schoolplanner.teacher.exception;
 
 public class EmailExistException extends RuntimeException {
-
     public EmailExistException(String email) {
-        String message = "Email %s already exist".formatted(email);
+        super("Email %s already exist".formatted(email));
+
     }
 }
 
