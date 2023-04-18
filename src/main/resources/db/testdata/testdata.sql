bf798318122eb3f6ffb0d28ab61684db3a24f7c6
Joanna Babinska <babinskaasia52@gmail.com>
2023-04-18 10:06:49 +0200

    modify testdata.sql

--- src/main/resources/db/testdata/testdata.sql
+++ src/main/resources/db/testdata/testdata.sql
@@ -2,4 +2,7 @@ INSERT INTO
     teacher(first_name, last_name, date_of_birth, hourly_wage, email, phone_number)
 VALUES
     ('Jan', 'Kowalski', '1999-10-12', 100, 'joanna@gmail.com', '889655425'),
-    ('Joanna', 'Nowak', '1999-06-21', 95, 'kacper@gamil.com', '962158745');
+    ('Joanna', 'Nowak', '1999-06-21', 95, 'kacper@gamil.com', '962158745'),
+    ('Marta', 'Żak', '1979-05-21', 100, 'marta.żak@gamil.com', '548896625'),
+    ('Bogumił', 'Sosonowski', '2000-01-05', 95, 'bogumił.sosnowski@gamil.com', '753369963'),
+    ('Ryszard', 'Akawa', '1996-02-21', 95, 'ryszard.Akawa@gamil.com', '789654123');
