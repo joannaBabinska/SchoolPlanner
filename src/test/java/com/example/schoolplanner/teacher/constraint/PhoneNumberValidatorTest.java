d8597d8c67dfa6f6ba4c6232a789b96b8268badd
Joanna Babinska <babinskaasia52@gmail.com>
2023-04-23 12:37:33 +0200

    add validatorTest

--- src/test/java/com/example/schoolplanner/teacher/constraint/PhoneNumberValidatorTest.java
+++ src/test/java/com/example/schoolplanner/teacher/constraint/PhoneNumberValidatorTest.java
@@ -1,9 +1,31 @@
 package com.example.schoolplanner.teacher.constraint;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PhoneNumberValidatorTest {
 
+    private PhoneNumberValidator phoneNumberValidator;
 
+    @BeforeEach
+    void setUp() {
+        phoneNumberValidator = new PhoneNumberValidator();
+    }
 
+    @Test
+    public void shouldReturnTrue() {
+        assertTrue(phoneNumberValidator.isValid("345345354", null));
+        assertTrue(phoneNumberValidator.isValid("25895114765", null));
+        assertTrue(phoneNumberValidator.isValid(null, null));
+    }
+
+    @Test
+    public void shouldReturnFalse() {
+        assertFalse(phoneNumberValidator.isValid("34534535", null));
+        assertFalse(phoneNumberValidator.isValid("258951147565", null));
+        assertFalse(phoneNumberValidator.isValid("juytssdff", null));
+        assertFalse(phoneNumberValidator.isValid("         ", null));
+    }
 }
